0d4f1622e7da1fa73319cce615909885dd894164
Edward Wibowo <wibow9770@gmail.com>
2022-02-26 16:53:05 +0800

    feat: implement player movement and camera follow

--- src/camera.rs
+++ src/camera.rs
@@ -1,13 +1,43 @@
+use crate::player::Player;
 use bevy::prelude::*;
 
 pub struct CameraPlugin;
 
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(spawn_camera);
+        app.add_startup_system(spawn_camera)
+            .add_system(follow_player);
     }
 }
 
+#[derive(Component)]
+struct MainCamera;
+
+impl MainCamera {
+    // Interpolation value for following the player
+    const INTERPOLATION: f32 = 0.1;
+}
+
 fn spawn_camera(mut commands: Commands) {
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands
+        .spawn_bundle(OrthographicCameraBundle::new_2d())
+        .insert(MainCamera);
+}
+
+fn follow_player(
+    mut camera_transform: Query<&mut Transform, With<MainCamera>>,
+    player_transform: Query<&Transform, (With<Player>, Without<MainCamera>)>,
+) {
+    let mut camera_transform = camera_transform.single_mut();
+    let player_transform = player_transform.single();
+
+    // Set camera translation without overwriting z-ordering
+    camera_transform.translation = camera_transform
+        .translation
+        .truncate()
+        .lerp(
+            player_transform.translation.truncate(),
+            MainCamera::INTERPOLATION,
+        )
+        .extend(0.0);
 }
--- src/player.rs
+++ src/player.rs
@@ -4,12 +4,12 @@ pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(spawn_player);
+        app.add_startup_system(spawn_player).add_system(move_player);
     }
 }
 
 #[derive(Component)]
-struct Player;
+pub struct Player;
 
 impl Player {
     const SIZE: f32 = 16.0;
@@ -31,4 +31,22 @@ fn spawn_player(mut commands: Commands) {
             ..SpriteBundle::default()
         })
         .insert(Player);
+}
+
+fn move_player(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut transform: Query<&mut Transform, With<Player>>,
+) {
+    let mut transform = transform.single_mut();
+
+    for key in keyboard_input.get_just_pressed() {
+        // Move player translation based on keyboard input
+        match key {
+            KeyCode::Left | KeyCode::A => transform.translation.x -= Player::SIZE,
+            KeyCode::Right | KeyCode::D => transform.translation.x += Player::SIZE,
+            KeyCode::Up | KeyCode::W => transform.translation.y += Player::SIZE,
+            KeyCode::Down | KeyCode::S => transform.translation.y -= Player::SIZE,
+            _ => {}
+        }
+    }
 }
